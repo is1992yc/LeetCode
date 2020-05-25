50412a82df5a2b46747a6b9a63744f70cebb68b2
yican <yican.pan@dragonsoftbravo.com>
2020-05-25 14:59:16 +0800

    init

---
+++ src/main/java/com/is1992yc/leetcode/LeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.is1992yc.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeApplication.class, args);
+    }
+
+}
